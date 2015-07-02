67589a2b73054790f05bebcb16cfd542f3f7b19c
didimitrov <di_dimitrov_@abv.bg>
2015-07-02 13:22:16 +0300

    Added ADO Entity models

--- ProjectTracking/ProjectTracking/Global.asax.cs
+++ ProjectTracking/ProjectTracking/Global.asax.cs
@@ -11,7 +11,7 @@ namespace ProjectTracking
     {
         protected void Application_Start()
         {
-            GlobalConfiguration.Configure(WebApiConfig.Register);
+            //GlobalConfiguration.Configure(WebApiConfig.Register);
         }
     }
 }
