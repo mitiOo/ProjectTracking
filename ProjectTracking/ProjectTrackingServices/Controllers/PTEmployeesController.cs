c2a1664354674e2e6739705017632c8af4b1184d
mitiOo <ivanov.d84@abv.bg>
2015-07-02 15:56:48 +0300

    Implement EmployeeRepository and PTEmploeesController, Fixing CROS(reinstall EF)

--- ProjectTracking/ProjectTrackingServices/Controllers/PTEmployeesController.cs
+++ ProjectTracking/ProjectTrackingServices/Controllers/PTEmployeesController.cs
@@ -1,35 +1,61 @@
 ﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
+using ProjectTrackingServices.Models;
 
 namespace ProjectTrackingServices.Controllers
 {
+    [EnableCors(origins: "http://localhost:55058", headers: "*", methods: "*")]
     public class PTEmployeesController : ApiController
     {
+      
         // GET: api/PTEmployeesController
-        public IEnumerable<string> Get()
+        [Route("api/ptemployees")]
+        public HttpResponseMessage Get()
         {
-            return new string[] { "value1", "value2" };
+            var employees = EmployeesRepository.GetAllEmployees();
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, employees);
+            return response;
         }
 
+       
+
         // GET: api/PTEmployeesController/5
-        public string Get(int id)
+        [Route("api/ptemployees/{id?}")]
+        public HttpResponseMessage GetEmployee(int id)
         {
-            return "value";
+            var emp = EmployeesRepository.GetEmployee(id);
+            var response = Request.CreateResponse(HttpStatusCode.OK, emp);
+            return response;
         }
 
         // POST: api/PTEmployeesController
-        public void Post([FromBody]string value)
+        [Route("api/employees")]
+        public HttpResponseMessage Post([FromBody]Employee e)
         {
+            var emp = EmployeesRepository.InsertEmployee(e);
+            var response = Request.CreateResponse(HttpStatusCode.OK, emp);  
+            return response;
         }
 
         // PUT: api/PTEmployeesController/5
-        public void Put(int id, [FromBody]string value)
+        [Route("api/employees")]
+        public HttpResponseMessage Put(Employee e)
         {
+            var emp = EmployeesRepository.UpdateEmployee(e);
+            var response = Request.CreateResponse(HttpStatusCode.OK, emp);
+            return response;
         }
 
         // DELETE: api/PTEmployeesController/5
-        public void Delete(int id)
+        [Route("api/employees")]
+        public HttpResponseMessage Delete(int id)
         {
+            var emp = EmployeesRepository.DeleteEmployee(id);
+            var response = Request.CreateResponse(HttpStatusCode.OK, emp);
+            return response;
         }
     }
 }
--- ProjectTracking/ProjectTrackingServices/Models/EmployeesRepository.cs
+++ ProjectTracking/ProjectTrackingServices/Models/EmployeesRepository.cs
@@ -1,11 +1,67 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using System.Runtime.Remoting.Messaging;
 
 namespace ProjectTrackingServices.Models
 {
     public class EmployeesRepository
     {
+        private static readonly ProjectTrackingDBEntities Db= new ProjectTrackingDBEntities();
+
+        //public static IList<Employee> GetAllEmployees()
+        //{
+        //    return Db.Employees.ToList();
+        //}
+
+        public static List<Employee> GetAllEmployees()
+        {
+            var query = from employee in Db.Employees
+                        select employee;
+            return query.ToList();
+        }
+
+        public static Employee GetEmployee(int id)
+        {
+            return Db.Employees.SingleOrDefault(x => x.EmployeeID == id);
+        }
+
+        public IList<Employee> SearchEmployeesByName(string serchedName)
+        {
+            return Db.Employees.Where(x => x.EmployeeName.Contains(serchedName)).ToList();
+        }
+
+        public static IList<Employee> InsertEmployee(Employee e)
+        {
+            Db.Employees.Add(e);
+            Db.SaveChanges();
+            return GetAllEmployees();
+        }
+
+        public static IList<Employee> UpdateEmployee(Employee e)
+        {
+            var employee = Db.Employees.SingleOrDefault(x => x.EmployeeID==e.EmployeeID);
+            if (employee != null)
+            {
+                employee.EmployeeID = e.EmployeeID;
+                employee.EMailID = e.EMailID;
+                employee.Designation = e.Designation;
+                employee.ContactNo = e.ContactNo;
+                employee.SkillSets = e.SkillSets;
+                employee.ManagerID = e.ManagerID;
+                employee.ProjectTasks = e.ProjectTasks;
+            }
+
+            Db.SaveChanges();
+            return GetAllEmployees();
+         }
+
+        public static IList<Employee> DeleteEmployee(int id)
+        {
+            var emp = Db.Employees.Find(id);
+            Db.Employees.Remove(emp);
+            Db.SaveChanges();
+            return GetAllEmployees();
+
+        }
     }
 }
